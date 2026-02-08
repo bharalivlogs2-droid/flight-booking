0f48112fbe8a1a227c4110a05ec0299ca96db5ce
Jyoti Bharali <bharalivlogs2@gmail.com>
2026-02-08 15:02:46 +0530

    Fix admin page server crash

--- app/api/bookings/route.ts
+++ app/api/bookings/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
 export async function GET() {
-  try {
-    const client = await clientPromise;
-    const db = client.db("test");
-    const bookings = await db.collection("bookings").find({}).toArray();
+  const client = await clientPromise;
+  const db = client.db();
 
-    return NextResponse.json(bookings);
-  } catch (error) {
-    return NextResponse.json({ error: "DB error" }, { status: 500 });
-  }
+  const bookings = await db
+    .collection("bookings")
+    .find({})
+    .toArray();
+
+  return NextResponse.json(bookings);
 }
